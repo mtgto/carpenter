e8a672104b7605004bcf7437bd8a338d79b5874f
mtgto <hogerappa@gmail.com>
2013-03-30 22:55:12 +0900

    Add bootstrap.

--- project/Build.scala
+++ project/Build.scala
@@ -13,9 +13,16 @@ object ApplicationBuild extends Build {
     anorm
   )
 
+  // Only compile the bootstrap bootstrap.less file and any other *.less file in the stylesheets directory
+  def customLessEntryPoints(base: File): PathFinder = (
+    (base / "app" / "assets" / "stylesheets" / "bootstrap" * "bootstrap.less") +++
+    (base / "app" / "assets" / "stylesheets" * "*.less")
+  )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature", "-Xlint", "-encoding", "UTF8"),
+    resolvers += "scala-dddbase Repository" at "http://sisioh.github.com/scala-dddbase/repos/release/",
+    lessEntryPoints <<= baseDirectory(customLessEntryPoints)
   )
 
 }
