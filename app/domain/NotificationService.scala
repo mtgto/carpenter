e49fa8765ae02e4f281b6a0eb7d5401f1a166538
mtgto <hogerappa@gmail.com>
2013-04-15 09:46:02 +0900

    Fix to specify encoding on IRC notification.

--- app/domain/NotificationService.scala
+++ app/domain/NotificationService.scala
@@ -11,6 +11,7 @@ object NotificationService extends NotificationService {
   override def notify(notification: Notification, message: String) = {
     val bot = new PircBotX
     bot.setName(notification.userName)
+    bot.setEncoding(notification.encoding)
     bot.connect(notification.hostname, notification.port)
     bot.joinChannel(notification.channelName)
     bot.sendNotice(notification.channelName, message)
